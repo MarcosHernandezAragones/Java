fd5f70e4434360299c45b58960526b969c86c4b6
Marcos <marcoshernandezaragones@gmail.com>
2020-11-04 12:30:42 +0100

    Empezando

---
+++ Ejercicios_1_0/src/Ejercicio_1_0_1.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Ejercicio_1_0_1 {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * 1) Dadas dos variables numéricas A y B, que el usuario debe teclear, se pide realizar un algoritmo
+		 * que intercambie los valores de ambas variables y muestre cuanto valen al final las dos variables
+		 * (recuerda la asignación).
+		 */
+
+		//Declarar variables e inicializarlas
+		//enteros
+		int num1=0;
+		int num2=0;
+		int num3=0;
+		
+		//reales
+		float real=0;
+		
+		//caracteres
+		String nombre="";
+		
+		//logicos
+		boolean bandera=true;
+		
+		//leer por teclado
+		Scanner leer= new Scanner(System.in);
+		
+		//Pedir por pantalla
+		System.out.println("Escribe tu Número");
+		num1=leer.nextInt();
+		
+		System.out.println("Escribe otro Número");
+		num2=leer.nextInt();
+		
+		num3=num1;
+		num1=num2;
+		num2=num3;
+		
+		
+		System.out.println("el primer número ahora es  "+num1+" y el segundo número es "+num2);
+		
+	}
+
+}
